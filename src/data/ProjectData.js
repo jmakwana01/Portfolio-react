1b662d443b16e0e24941bb4e17388c33b641104c
jmakwana01 <jaymakwanaworks@gmail.com>
2021-10-11 14:09:22 +0530

    second commit

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -1,7 +1,7 @@
 // --------------------------------------- Project List
 export const ProjectList = [
   {
-    img: "https://user-images.githubusercontent.com/87368354/134036718-cca0ec3a-8694-4275-8a09-ba0821faa4d7.jpeg",
+    img: "https://user-images.githubusercontent.com/87368354/136758979-f844cabf-ac98-43a4-b4f6-64f98d5fb332.png",
     title: "EthSwap",
     description: "Ether-to-Erc20-token-Exchange",
     tech_stack: "Solidity,React,Javascript",
@@ -9,12 +9,12 @@ export const ProjectList = [
     demo_url: "https://sad-meninsky-ce295f.netlify.app",
   },
   {
-    img: "https://user-images.githubusercontent.com/87368354/134036718-cca0ec3a-8694-4275-8a09-ba0821faa4d7.jpeg",
+    img: "https://user-images.githubusercontent.com/87368354/136759360-47c506ed-3804-4c24-9fc1-ac0d8148648b.png",
     title: "EthSwap",
-    description: "Ether-to-Erc20-token-Exchange",
-    tech_stack: "Solidity,React,Javascript",
-    github_url: "https://github.com/jmakwana01/Ether-to-Erc20-token-Exchange/",
-    demo_url: "https://sad-meninsky-ce295f.netlify.app",
+    description: "Decentralized app for election",
+    tech_stack: "Blockchain,Solidity,Angularjs,Javascript",
+    github_url: "https://github.com/jmakwana01/Decentralized-app-for-Election",
+    demo_url: "https://github.com/jmakwana01/Decentralized-app-for-Election",
   },
   {
     img: "https://user-images.githubusercontent.com/87368354/134036718-cca0ec3a-8694-4275-8a09-ba0821faa4d7.jpeg",
